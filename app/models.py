10150c6008ecab991d29cb4b82e85189f0deb201
Guillermo García López <memogl25@gmail.com>
2018-11-03 21:33:38 -0600

    add models for WordClass and DuolingoWord

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from app import db
+
+class WordClass(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32))
+    duolingo_word = db.relationship('DuolingoWord', backref='word class', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Word class {}>'.format(self.name)
+
+class DuolingoWord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(32))
+    pronunciation = db.Column(db.String(32))
+    word_class_id = db.Column(db.Integer, db.ForeignKey('word_class.id'))
--- english-sounds.py
+++ english-sounds.py
@@ -1,1 +1,8 @@
-from app import app
+from app import app, db
+from app.models import WordClass, DuolingoWord
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db,
+            'WordClass': WordClass,
+            'DuolingoWord': DuolingoWord}
