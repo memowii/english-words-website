2d08f7f3c18ccc4a60a5b23dc4413ddbc5c8e823
Guillermo García López <memogl25@gmail.com>
2018-11-06 21:11:48 -0600

    show words with/without diphthongs

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from app import app
 from app.models import WordClass, DuolingoWord
 from app.utils import *
 
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -19,14 +20,18 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+
 @app.route('/ipa/<ipa_symbol>')
 def ipa(ipa_symbol):
+    words_with_ipa = DuolingoWord \
+        .query \
+        .filter(DuolingoWord.pronunciation.contains(ipa_symbol)) \
+        .all()
+
     if is_diphthong(ipa_symbol):
-        words_with_ipa = DuolingoWord\
-                         .query\
-                         .filter(DuolingoWord.pronunciation.contains(ipa_symbol))\
-                         .all()
         return render_template('ipa.html', ipa_symbol=ipa_symbol, words_with_ipa=words_with_ipa)
-
-
-    # print(words_with_ipa)
+    else:
+        words_without_diphthongs = \
+            list(filter(lambda duolingo_word: not has_diphthong(ipa_symbol, duolingo_word.pronunciation),
+                        words_with_ipa))
+        return render_template('ipa.html', ipa_symbol=ipa_symbol, words_with_ipa=words_without_diphthongs)
